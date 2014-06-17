bba7f022f41a7b2528bfabae0ae46a3299c45dc5
lnv326 <lnv326@gmail.com>
2014-06-17 19:50:19 +0400

    First simple UnitTest

---
+++ src/Tests/Controller/ShowControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Site\GalleryBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ShowControllerTest extends WebTestCase {
+	
+	public function testIndex() {
+		$client = static::createClient();		
+		$crawler = $client->request('GET', '/hello/Fabien');		
+		$this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+	}
+}
+?>
