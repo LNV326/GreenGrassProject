82fdb5950db3bf2965b37bfa0928ffdc185207bb
LNV326 <lnv326@gmail.com>
2014-06-18 22:10:24 +0400

    Working UnitTest

--- src/Tests/Controller/ShowControllerTest.php
+++ src/Tests/Controller/ShowControllerTest.php
@@ -3,13 +3,30 @@
 namespace Site\GalleryBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Crawler;
 
 class ShowControllerTest extends WebTestCase {
 	
+	var $CATS = array('Rivals',
+			'Porsche',
+			'Most Wanted (CG)',
+			'The Run');
+	
 	public function testIndex() {
-		$client = static::createClient();		
-		$crawler = $client->request('GET', '/hello/Fabien');		
-		$this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+		$client = static::createClient();
+		$container = $client->getContainer();
+		$cats = $container->get('doctrine')->getManager()->getRepository('SiteGalleryBundle:ImageCategory')->getCats();
+		
+		$crawler = $client->request('GET', '/gallery/');	
+		$arr = $crawler->filter('div.gallery_thumb_name')->extract(array('_text'));
+		foreach($cats as $elm) {
+			$this->assertContains($elm->getName(), $arr);
+		}
+// 		$this->assertSame($arr, $this->CATS);
+		//$this->assertArrayHasKey("Porsche"+$arr[1], $arr);
+		//$this->assertTrue($crawler->filter('html:contains("Porsche1")')->count() > 0);
 	}
+	
+	
 }
 ?>
